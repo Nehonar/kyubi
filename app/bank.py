a07cb80b8a90e9fe2e6f363e3da66cd335cb6755
Nehonar <benito.avar@gmail.com>
2021-06-10 21:23:02 +0200

    Add logs and refactor

--- app/bank.py
+++ app/bank.py
@@ -1,23 +1,41 @@
 import logging
-from app.login import Login
-from app.customers import Customer
-from app.accounts import Account
+import requests
+
+from requests.sessions import session
+
+from app import Status
+from app.customer import Customer
+from app.account import Account
+from app.statement import Statement
 from bs4 import BeautifulSoup
+
+log = logging.getLogger('Bank')
 
 class Bank:
     """ 
         This class manage the structure of data bank
     """
-    log = logging.getLogger('Bank')
+    _html_accounts = None
+    _session = None
+    _url = "https://test.unnax.com/"
 
-    def login(self):
-        # self.log.info('Start login')
-        (session, html_accounts) = Login.login_session(self)
+    def __init__(self):
+        self._session = requests.session()
 
-        return (session, html_accounts)
+    def login(self, auth_data):
+        log.info('Start login')
+        response =  self._session.post(self._url + 'login', data=auth_data)
 
-    def get_accounts(self,session):
-        html_accounts = BeautifulSoup(self, 'html.parser')
+        if "Login" in response.text or not response.ok:
+            log.error("Failed login")
+            return Status.LoginError
+
+        self._html_accounts = response.text
+        log.info("Login OK")
+        return Status.LoginOk
+
+    def get_accounts(self):
+        html_accounts = BeautifulSoup(self._html_accounts, 'html.parser')
         list_accounts = html_accounts.find_all('li', {'class': 'collection-item'})
         
         accounts = []
@@ -29,13 +47,28 @@ class Bank:
             account.number =    data_account.find('p').getText()[0:20]
             account.currency =  data_account.p.span.string[0] # TODO create table conversion
             account.balance =   data_account.p.span.string[1:]
+            # In case to crush statement or customer pass to next account
+            try:
+                account.statements = self._get_statements(data_account.find_all('a')[0]['href'])
+                account.customers = self._get_customers()
+            except Exception as e:
+                log.error(e)
+                continue
+
             accounts.append(account)
 
         return accounts
     
-    def get_customers(self):
-        resp = Login.login_customers(self)
-        html_customers = BeautifulSoup(resp, 'html.parser')
+    def _get_customers(self):
+        resp = self._session.get(self._url + 'customer')
+
+        if not resp.ok:
+            log.error("Error customers")
+            raise Exception("Error, no existen customers")
+        
+        log.info("Get customers")
+
+        html_customers = BeautifulSoup(resp.text, 'html.parser')
         list_customers = html_customers.find_all('ul', {'class': 'collection'})
         
         customers = []
@@ -43,28 +76,33 @@ class Bank:
         for data_customer in list_customers:
             customer = Customer()
 
-            customer.qty =              len(list_customers)
-            customer.name =             data_customer.find_all('li', {'class': 'collection-header'})[0].string
+            customer.qty =  len(list_customers)
+            customer.name = data_customer.find_all('li', {'class': 'collection-header'})[0].string
             # Data items
-            data_item = data_customer.find_all('li', {'class': 'collection-item'})
-            customer.address =          data_item[2].string
-            customer.emails =           data_item[1].string
-            customer.phones =           data_item[0].string
+            data_item =         data_customer.find_all('li', {'class': 'collection-item'})
+            customer.address =  data_item[2].string
+            customer.emails =   data_item[1].string
+            customer.phones =   data_item[0].string
             # Special items
-            # TODO search other option to find this info
-            if len(data_item) >= 4:
-                customer.doc = data_item[3].string
-            else:
-                customer.doc = "None"
-
-            if len(data_item) >= 5:
-                customer.participation = data_item[4].string
-            else:
-                customer.participation = "Titular"
+            # TODO this info none exist in customer
+            customer.doc = None
+            customer.participation = "Titular"
             
             customers.append(customer)
         
         return customers
 
-    def get_statements(self):
-        print(self)
+    def _get_statements(self, link_statement):
+        resp = self._session.get(self._url + link_statement)
+        html_statements = BeautifulSoup(resp.text, 'html.parser')
+        list_statements = html_statements.find_all('tr')
+        
+        statements = []
+
+        for data_statement in list_statements:
+            statement = Statement()
+
+            statement.qty = len(data_statement)
+            statements.append(statement)
+        
+        return statements
--- read.py
+++ read.py
@@ -1,55 +1,54 @@
 import argparse
-from app.login import Login
+import logging
+
 from app.bank import Bank
+from app import Status
 
-parser = argparse.ArgumentParser(description='Login through username and password')
-parser.add_argument('--username', required=True, help='Username')
-parser.add_argument('--password', required=True, help='Password')
-args = parser.parse_args()
+logging.basicConfig(level=logging.DEBUG)
 
-auth_data = {
-    'username': args.username, 
-    'password': args.password
-}
+log = logging.getLogger("read")
 
-(session, html_accounts) = Bank.login(auth_data)
+if __name__ == "__main__":
 
-accounts = Bank.get_accounts(html_accounts, session)
-customers = Bank.get_customers(session)
+    # Get data authentication
+    parser = argparse.ArgumentParser(description='Login through username and password')
+    parser.add_argument('--username', required=True, help='Username')
+    parser.add_argument('--password', required=True, help='Password')
+    args = parser.parse_args()
+    auth_data = {
+        'username': args.username, 
+        'password': args.password
+    }
 
-print(f"Accounts ( {len(accounts)} )", end="\n\n     ")
-for account in accounts:
-    # Account data
-    print("Account data:",                  end="\n         ")
-    print("Name: ",     account.name,       end="\n         ")
-    print("Number: ",   account.number,     end="\n         ")
-    print("Currency: ", account.currency,   end="\n         ")
-    print("Balance: ",  account.balance,    end="\n\n     ")
-    # Total customers
-    for customer in customers:
-        print("Total customers: ", customer.qty,            end="\n         ")
-        print("Customer data:",                             end="\n              ")
-        print("Name: ", customer.name,                      end="\n              ")
-        print("Participation: ", customer.participation,    end="\n              ")
-        print("Doc: ", customer.doc,                        end="\n\n              ")
-        print("Address", customer.address,                  end="\n\n              ")
-        print("Email: ", customer.emails,                    end="\n              ")
-        print("Phones: ", customer.phones,                  end="\n\n     ")
+    # Bank petitions
+    bank = Bank()
 
+    login_status = bank.login(auth_data)
+    if login_status == Status.LoginError:
+        log.error("Fail in login get out")
+        exit(1)
 
-# print(GetStatements(session, "statements/1"))
+    accounts = bank.get_accounts()
 
-
-
-# account_json = html_to_json.convert(account.text)
-# account_json = account_json['html'][0]['body'][0]['div'][0]['div'][0]['ul'][0]['li']
-
-# print(f"Accounts ( {len(account_json)} )", end="\n     ")
-
-# for account in account_json:
-#     resp_customers = session.get('https://test.unnax.com/customer')
-#     resp_statement = session.get('http://test.unnax.com/' + account['a'][0]['_attributes']['href'])
-#     statement_json = html_to_json.convert(resp_statement.text)
-#     resp_customers = html_to_json.convert(resp_customers.text)
-#     data_customers = resp_customers['html'][0]['body'][0]['div'][0]['div'][0]['ul']
-#     structuring(resp_customers, statement_json, data_customers, account)
+    # Print info accounts
+    print(f"Accounts ( {len(accounts)} )", end="\n\n     ")
+    for account in accounts:
+        # Account data
+        print("Account data:",                  end="\n         ")
+        print("Name: ",     account.name,       end="\n         ")
+        print("Number: ",   account.number,     end="\n         ")
+        print("Currency: ", account.currency,   end="\n         ")
+        print("Balance: ",  account.balance,    end="\n\n     ")
+        # Total customers
+        for customer in account.customers:
+            print("Total customers: ", customer.qty,            end="\n         ")
+            print("Customer data:",                             end="\n              ")
+            print("Name: ", customer.name,                      end="\n              ")
+            print("Participation: ", customer.participation,    end="\n              ")
+            print("Doc: ", customer.doc,                        end="\n\n              ")
+            print("Address", customer.address,                  end="\n\n              ")
+            print("Email: ", customer.emails,                    end="\n              ")
+            print("Phones: ", customer.phones,                  end="\n\n     ")
+        for statement in account.statements:
+            print("statements")
+        
