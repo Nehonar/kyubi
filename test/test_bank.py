2a5a1c16beb237ff653aa4af3258de81c11f80cc
Nehonar <benito.avar@gmail.com>
2021-06-20 00:36:33 +0200

    Add new tests and remove a old 'TODO'

--- test/test_bank.py
+++ test/test_bank.py
@@ -2,38 +2,61 @@ import unittest
 import json
 
 from app.bank import Bank
+from bs4 import BeautifulSoup
 
 with open('test/config.json', 'r') as file:
     config = json.load(file)
 
+username = config["TESTING_LOGIN"]["USERNAME"]
+password = config["TESTING_LOGIN"]["PASSWORD"]
+data_fake = config["TESTING_LOGIN"]["DATA_FAKE"]
+
+bank = Bank()
+
 class TestMethods(unittest.TestCase):
 
+    # Test login
     def test_bank_loguin_ok(self):
         auth_data = {
-            'username': config["TESTING_LOGIN"]["USERNAME"],
-            'password': config["TESTING_LOGIN"]["PASSWORD"]
+            'username': username,
+            'password': password
         }
-        bank = Bank()
         
         self.assertEqual(bank.login(auth_data), 1)
 
     def test_bank_login_fake_user(self):
         auth_data = {
-            'username': config["TESTING_LOGIN"]["DATA_FAKE"],
-            'password': config["TESTING_LOGIN"]["PASSWORD"]
+            'username': data_fake,
+            'password': password
         }
-        bank = Bank()
 
         self.assertEqual(bank.login(auth_data), 2)
     
     def test_bank_login_fake_pass(self):
         auth_data = {
-            'username': config["TESTING_LOGIN"]["USERNAME"],
-            'password': config["TESTING_LOGIN"]["DATA_FAKE"]
+            'username': username,
+            'password': data_fake
         }
-        bank = Bank()
 
         self.assertEqual(bank.login(auth_data), 2)
+
+    # Test get_accounts
+    def test_get_accounts(self):
+        accounts = bank.get_accounts()
+
+        self.assertEqual(accounts[0].name, "Cuenta personal")
+
+    # Test _get_customers
+    def test_get_customers(self):
+        customers = bank._get_customers()
+
+        self.assertEqual(customers[0].name, "Pepito Perez")
+
+    # Test _get_statements
+    def test_get_statements(self):
+        statements = bank._get_statements("statements/1")
+
+        self.assertEqual(statements[0].date, "05/07/2018")
 
 if __name__ == '__main__':
     unittest.main()
